44fcade012391558ca81bea4d537a0a4db1a63c9
icharle <icharle@live.com>
2018-10-28 17:44:15 +0800

    题目、答案、图片爬取

--- rkpass/spiders/rkpassSpider.py
+++ rkpass/spiders/rkpassSpider.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from bs4 import BeautifulSoup
+import re
 
 
 class RkpassspiderSpider(scrapy.Spider):
@@ -7,5 +9,19 @@ class RkpassspiderSpider(scrapy.Spider):
     allowed_domains = ['http://www.rkpass.cn/']
     start_urls = []
 
+    for i in range(36, 37):
+        start_urls.append('http://www.rkpass.cn/tk_timu/6_553_' + str(i) + '_xuanze.html')
+
     def parse(self, response):
-        pass
+        bsObj = BeautifulSoup(response.text)
+        pictures = bsObj.findAll('img', {'src': re.compile('.*\/.*\/.*\/.*\/.*\.png')})
+        titles = bsObj.find_all('span', {'class': 'shisi_text'})
+        for txt in titles:
+            txt = txt.text
+            txt = re.sub('\s*', '', txt)
+            print(txt)
+
+        for picture in pictures:
+            url = re.compile('http\:\/\/www\.rkpass\.cn\/ruankao\_work\_version\_0103\/userfile\/image\/(.*)')
+            res = url.search(picture['src']).groups()[0]
+            print(res)
