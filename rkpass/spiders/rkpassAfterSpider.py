3355bda10331bf5ac562fff2553487c5d05535ab
icharle <icharle@live.com>
2018-10-30 20:00:47 +0800

    下午题目及答案获取

--- rkpass/spiders/rkpassAfterSpider.py
+++ rkpass/spiders/rkpassAfterSpider.py
@@ -8,7 +8,7 @@ class RkpassafterspiderSpider(scrapy.Spider):
     allowed_domains = ['www.rkpass.com']
     start_urls = []
 
-    for i in range(2, 3):
+    for i in range(5, 6):
         start_urls.append('http://www.rkpass.cn/tk_timu/6_580_' + str(i) + '_anli.html')
 
     def parse(self, response):
@@ -17,6 +17,18 @@ class RkpassafterspiderSpider(scrapy.Spider):
             "'")  # 该题目id 用于整理答案
         question = "".join(response.xpath(".//table/tr[2]/td/span[@class='shisi_text']//text()").extract())  # 题目
 
-        wenti = "".join(response.xpath(".//table/tr/td/text()").extract()) # tr[] 4,6,8,10
+        wenti = "".join("".join(response.xpath(".//table/tr/td/text()").extract()).split())  # tr[] 4,6,8,10
 
-        print(wenti)
+        # print(wenti.split('。（'))  # 分割题目
+
+        # print(question)
+
+        url = 'http://www.rkpass.cn/tk_jiexi.jsp?product_id=201806131544056256919&tixing=anli&answer=&paper_id=580&tihao=1&cache=false'
+        yield scrapy.Request(url, callback=self.parse_detail, dont_filter=True)
+
+    def parse_detail(self, response):
+        img = response.xpath(".//td/img/@src").extract()
+        # print(img)
+        answerAnalysis = "".join(response.xpath(".//table/tr/td//text()").extract())  # 答案解析
+        print("".join(answerAnalysis.split('软考在线[rkpass.cn]答案解析：')))
+
