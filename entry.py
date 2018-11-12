f5a7353a92d4f79fb5e839398fc859f54ed2c4e8
icharle <icharle@live.com>
2018-11-13 01:35:43 +0800

    添加爬取信息系统管理工程师题库

--- entry.py
+++ entry.py
@@ -1,2 +1,2 @@
 from scrapy.cmdline import execute
-execute(['scrapy','crawl','mediaMorningSpider'])
+execute(['scrapy','crawl','xxxtMorningSpider'])
--- rkpass/items.py
+++ rkpass/items.py
@@ -123,3 +123,15 @@ class mediaMorningItem(scrapy.Item):
     answer = scrapy.Field()  # 答案
     answeranalysis = scrapy.Field()  # 答案解析
     field = scrapy.Field()  # 考试场次 20181代表2018上半年考试
+
+# 信息系统管理工程师上午题
+class xxxtMorningItem(scrapy.Item):
+    question = scrapy.Field()  # 题目
+    questionImg = scrapy.Field()  # 题目图片
+    optiona = scrapy.Field()  # 选项A
+    optionb = scrapy.Field()  # 选项B
+    optionc = scrapy.Field()  # 选项C
+    optiond = scrapy.Field()  # 选项D
+    answer = scrapy.Field()  # 答案
+    answeranalysis = scrapy.Field()  # 答案解析
+    field = scrapy.Field()  # 考试场次 20181代表2018上半年考试
