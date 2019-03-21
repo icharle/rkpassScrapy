edb6d7dd7c866d00852a5c1fdb32ddbbd66960c7
icharle <icharle@live.com>
2019-03-21 13:16:53 +0800

    完善中级嵌入式系统设计师知识点分类以及题号

--- entry.py
+++ entry.py
@@ -1,2 +1,2 @@
 from scrapy.cmdline import execute
-execute(['scrapy','crawl','rjpcsMorningSpider'])
+execute(['scrapy','crawl','qrsMorningSpider'])
--- rkpass/items.py
+++ rkpass/items.py
@@ -114,6 +114,9 @@ class qrsMorningItem(scrapy.Item):
     answer = scrapy.Field()  # 答案
     answeranalysis = scrapy.Field()  # 答案解析
     field = scrapy.Field()  # 考试场次 20181代表2018上半年考试
+    questionNum = scrapy.Field()  # 题号
+    knowledgeOne = scrapy.Field()  # 当前题目一级分类
+    knowledgeTwo = scrapy.Field()  # 当前题目二级分类
 
 # 电子商务设计师上午题
 class dzswMorningItem(scrapy.Item):
